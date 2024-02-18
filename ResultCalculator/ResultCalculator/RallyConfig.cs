31b32dee16cd8ef096ffefea6b08260886519f5b
sunny-icpl <sunnym@infopercept.com>
2024-02-18 12:16:43 +0530

    Add RallyConfig

---
+++ ResultCalculator/ResultCalculator/RallyConfig.cs
@@ -0,0 +1,14 @@
+﻿internal class RallyConfig
+{
+    public required string TableName { get; set; }
+    
+    public required int Year { get; set; }
+    
+    public required DateOnly Date { get; set; }
+    
+    public required int EarlyPenalty { get; set; }
+    
+    public required int LatePenalty { get; set; }
+    
+    public required int MissedPenalty { get; set; }
+}
